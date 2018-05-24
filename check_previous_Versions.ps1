bc746aabdd1cb76f82b82f7d7efd23d2faecb51a
SvenFa <github@thenextdomain.de>
2018-05-24 16:46:47 +0200

    initial upload
    First upload

---
+++ check_previous_Versions.ps1
@@ -0,0 +1,156 @@
+# Written by Sven Falk (U710227) - 23~25 of May 2018
+# Description: This script will search for installed Software and is able to remove it if a correct uninstall string is found in the registry
+# ------ ToDo:
+# - Return Exitcodes of MSI to Matrix-Package for better handling?
+# - Return Errormessages to Matrix-Package for better log-entries in Empirum
+# - Search for software after uninstallation to make sure uninstallation was successful
+#
+# ------------------------------------------------------- Define environment -------------------------------------------------------
+# Param has to be the first line!
+# Defines the parameters which are given by calling this script:
+# e.g.: .\check_previous_Versions.ps1 -software "7-Zip" -version "16.01" -uninstall "yes" -debug 1
+param (
+    [string]$software = "",
+    [string]$version = "0.0",
+    [string]$uninstall = "no",
+    [int]$debug = 0
+)
+
+# ---- Exit Codes ----
+# Setup-routines will exit with their own exit-codes.
+# Define some custom exit-codes for this script.
+#   11000 | Software not found - exited without any action
+#   11001 | Undefined error
+#   11002 | removed found software-version
+#   11003 | found previous version but could not identify uninstall-routine
+#   11010 | Found Inno-setup but uninstall-exe was not found
+
+# ---- Debugging ----
+# Enable debugging (1) or disable (0)
+# Powershelldebugging:
+Set-PSDebug -Trace 0
+# Enable Debug-Write-Host-Messages:
+$DebugMessages = $debug
+
+# ------------------------------------------------------- End definition of environment ---------------------------------------------------
+
+
+# ------------------------------------------------------- Define functions ----------------------------------------------------------------
+
+function endscript{
+    # Debug info:
+    if ($DebugMessages -eq "1") {Write-Host "End of script"}
+    }
+
+# Define what to do with found entries. Put it into a function so it can be called for 32 and 64-bit searches
+function do_compare{
+
+    # Debug info:
+    if ($DebugMessages -eq 1) {Write-Host "--- Beginning of loop ---`nFound" $_.DisplayName "in version" $_.DisplayVersion}
+
+    # Check if found software is matrix-package:
+    if ("$_." -like "*Setup.inf*") { 
+        # Debug info:
+        if ($DebugMessages -eq "1") {Write-Host "This is a matrix package entry and not the installed software `nSkipping it."}
+
+        # Debug info:
+        if ($DebugMessages -eq "1") {Write-Host "--- End of loop ---`n"}
+        # Skip this found software and exit the function (but not the whole script)
+        return;
+    }
+
+    # Compare installed version
+    if ([System.Version]$_.DisplayVersion -lt [System.Version]"$version") {
+
+        # Debug info:
+        if ($DebugMessages -eq "1") {Write-Host "Found older version of" $_.DisplayName "(Given parameter version:" $version "> Installed version:" $_.DisplayVersion ")"}
+
+        # Checking uninstall-string type:
+        if ($_.UninstallString -match "MsiExec.exe") {
+
+            ##------------------------------------------------------- MSI Uninstallation -------------------------------------------------------
+            # Debug info:
+            if ($DebugMessages -eq "1") {Write-Host "Found MSI-Uninstallstring."}
+
+            # Modify '/I' argument to '/X'
+            if ($_.UninstallString -match "MsiExec.exe /I") {
+                $_.UninstallString = $_.UninstallString -replace "/I","/X"
+                # Debug info:
+                if ($DebugMessages -eq "1") {Write-Host "Found MSI-UninstallString and replaced /I with /X - New UninstallString:" $_.UninstallString}
+            }
+
+            # Debug Info:
+            if ($DebugMessages -eq "1") {Write-Host "Found UninstallString and extracted the parameters:" $_.UninstallString}
+
+            if ($_.UninstallString -match "MsiExec.exe /X") {
+
+                # Extract arguments to uninstall (We have to define arguments separated to use start-process)
+                $_.UninstallString = $_.UninstallString -replace "MsiExec.exe ",""
+                
+                Start-Process MsiExec.exe -wait -ArgumentList "$($_.UninstallString)","/qn","/log $env:Windir\Temp\UNINSTALLEDTHIS.log" -PassThru
+                if ($DebugMessages -eq "1") {Write-Host "Ran MsiExec.exe and got errorlevel:" $LASTEXITCODE}
+                
+                # Check returncode of msiexec. If it's not 0, exit this script.
+                if ($LASTEXITCODE -ne "0") { exit $LASTEXITCODE } else {return;}
+            }
+            ##------------------------------------------------------- End MSI Uninstallation ----------------------------------------------------
+
+        } elseif ($_.UninstallString -match "unins000.exe") {
+
+            ##------------------------------------------------------- Inno Uninstallation -------------------------------------------------------
+            # Debug info:
+            if ($DebugMessages -eq "1") {Write-Host "Found Inno-Setup uninstallation"}
+            
+            # Check if uninstallation file still exists:
+            # Test-Path does not like quotes, so we will trim them and save the path into a new variable:
+            $InnoFilePath=$_.UninstallString.Replace("`"","")
+
+
+            if($uninstall -eq "yes") {
+                # Debug info:
+                if ($DebugMessages -eq "1") {Write-Host "Uninstallation activated via arguments"}
+                if(Test-Path -Path $InnoFilePath) {
+                    # Debug info:
+                    if ($DebugMessages -eq "1") {Write-Host "Inno-Setup-File exists in" $_.UninstallString }
+
+                    Start-Process "$InnoFilePath" -ArgumentList "/VERYSILENT","/SUPPRESSMSGBOXES","/LOG=$env:Windir\Temp\UNINSTALLEDTHIS.log"
+                    # Check returncode of uninstallation-file. If it's not 0, exit this script.
+                    if ($LASTEXITCODE -ne "0") { exit $LASTEXITCODE } else {return;}
+                }
+            } else {
+                # Debug info:
+                if ($DebugMessages -eq "1") {Write-Host "Could not find Inno-Setup-File in" $_.UninstallString }
+                exit 11010
+            }
+
+            ##------------------------------------------------------- End Inno Uninstallation -------------------------------------------------------
+        } else {
+            # Debug info:
+            if ($DebugMessages -eq "1") {Write-Host "Found old version of software but could not identify uninstallation-routine" }
+            exit 11003
+        }
+    } else {
+        # Debug info:
+        if ($DebugMessages -eq "1") {Write-Host "No older version of" $_.DisplayName "found. (Given parameter version:" $version " - Installed version:" $_.DisplayVersion ")"}
+        exit 11000
+    }
+
+    # Debug info:
+    if ($DebugMessages -eq "1") {Write-Host "--- End of loop ---`n"}
+
+}
+# ------------------------------------------------------- End definition of functions ---------------------------------------------------
+#
+#
+# -------------------------------------------------------------------- Tasks  -------------------------------------------------------------
+# Search 32-Bit Software
+Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Where-Object {$_.DisplayName -like "*$software*"} | ForEach-Object -process { 
+   do_compare
+}
+
+# 64-Bit Software
+Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* | Where-Object {$_.DisplayName -like "*$software*"} | ForEach-Object -process { 
+   do_compare
+}
+
+endscript
